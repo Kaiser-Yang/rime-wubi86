3a7e1777e115064c30ea5737aedf362097ec7a88
Kaiser-Yang <624626089@qq.com>
2025-01-05 14:57:54 +0800

    Remove unfishied words

--- temp.py
+++ temp.py
@@ -2,39 +2,29 @@
 
 def main():
     file = open('./wubi.dict.yaml', 'r+', encoding='utf-8')
-    log_file = open('./removed.txt', 'w', encoding='utf-8')
-    code_dict = dict()
-    character_dict = dict()
+    log_file = open('./unfinished.txt', 'w', encoding='utf-8')
+    code_set = set()
+    unfinished_lines = set()
     lines = file.readlines()
     for line in lines:
         words = line.split('\t')
-        if len(words) < 2:
+        if len(words) < 4:
             continue
         character = words[0]
         code = words[1]
+        # [:-1] to remove the '\n' at the end of the line
+        stem = words[3][:-1]
         if len(character) != 1:
             continue
-        if code in code_dict:
-            continue
-        code_dict[code] = character
-        if character in character_dict:
-            character_dict[character].append(code)
-        else:
-            character_dict[character] = [code]
-    for character in character_dict:
-        character_dict[character].sort(key=lambda x: len(x))
+        if code in code_set and code != stem:
+            unfinished_lines.add(line)
+        code_set.add(code)
+    for line in unfinished_lines:
+        log_file.write(line)
     new_lines = []
     for line in lines:
-        words = line.split('\t')
-        if len(words) < 2:
+        if line not in unfinished_lines:
             new_lines.append(line)
-            continue
-        character = words[0]
-        code = words[1]
-        if code not in code_dict or code_dict[code] != character or character not in character_dict or character_dict[character][0] == code:
-            new_lines.append(line)
-        else:
-            log_file.write('removed: ' + line + 'reason : ' + character_dict[character][0] + '\n')
     file.seek(0)
     file.truncate()
     file.writelines(new_lines)
