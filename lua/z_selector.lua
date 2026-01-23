d721ef20846cf3fd9f91f6ad6ef030f50782ce57
kaiser <kaiserqzyue@foxmail.com>
2026-01-23 19:30:22 +0800

    feat: commit number by z_selector, use , to commit when no previous page

--- lua/z_selector.lua
+++ lua/z_selector.lua
@@ -5,28 +5,19 @@ local accept = 1
 local pass_to_next = 2
 
 local is_desktop = true
-local shift_pressed
 
 local function z_selector(key_event, env)
     local context = env.engine.context
     local input = context.input
-    if key_event.keycode == 65505 then -- Shift key
-        if not key_event:release() then
-            shift_pressed = true
-        elseif shift_pressed then
-            -- For single shift key, we commit the text
-            env.engine:commit_text(input)
-            context:clear()
-            shift_pressed = nil
-        end
+    local is_number = key_event.keycode >= 48 and key_event.keycode <= 57
+    if (not input or #input == 0) and is_number then
+        env.engine:commit_text(string.char(key_event.keycode))
         return accept
     end
-    shift_pressed = nil
     if key_event:release() or not input or #input == 0 then return pass_to_next end
     local composition = context.composition:back()
     local dest = 9999
     local page_size = env.engine.schema.page_size
-    local is_number = key_event.keycode >= 48 and key_event.keycode <= 57
     if is_number then
         dest = key_event.keycode - 48
         if dest == 0 then dest = 10 end -- 0 for select the 10-th item
@@ -40,9 +31,16 @@ local function z_selector(key_event, env)
         -- On desktop, we use ';' to select the second item;
         -- On phone, we use '/' to select the second item;
         dest = 2
-    elseif key_event.keycode == 44 and composition.selected_index > page_size then
+    elseif key_event.keycode == 44 then
         -- ',' is used to go back one page
-        return pass_to_next
+        if composition.selected_index >= page_size then
+            return pass_to_next
+        elseif context:has_menu() and composition.selected_index < page_size then
+            -- When there is no previous page, we commit the first item with punctuation
+            env.engine:commit_text(composition.menu:get_candidate_at(0).text .. '，')
+            context:clear()
+            return accept
+        end
     elseif key_event.keycode == 46 then
         -- '.' is used to go forward one page
         if context:has_menu() and composition.menu:candidate_count() < page_size then
